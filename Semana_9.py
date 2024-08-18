af8370fa21eb94c1b2b6039b106cdf67933f057f
ar.poncem <ar.poncem@uea.edu.ec>
2024-08-17 19:57:54 -0500

    Ver. 2

--- Semana_9.py
+++ Semana_9.py
@@ -1,3 +1,21 @@
 # Semana 9
 # Tarea: Sistema de Gestión de Inventarios
 
+
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        # Atributos del producto
+        self._id_producto = id_producto  # ID único del producto
+        self._nombre = nombre  # Nombre del producto
+        self._cantidad = cantidad  # Cantidad en inventario
+        self._precio = precio  # Precio del producto
+
+    # Getters y setters para cada atributo
+    def get_id_producto(self):
+        return self._id_producto
+
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, nombre):
+        self._nombre = nombre
